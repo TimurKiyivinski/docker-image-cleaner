867c2d2a0036a7c61e126da007b7c8f0c42e6b4a
Timothy Kiyui <tkiyui@idsdatanet.com>
2016-11-17 10:53:53 +0800

    [REFACTOR] Use Docker conntection to communicate with Docker [ADD] dockerode as dependency

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
+'use strict'
+
+const fs = require('fs')
 const exec = require('child_process').exec
+const Docker = require('dockerode')
+
+const env = JSON.parse(fs.readFileSync('env.json', 'utf8'))
 
 ;(function () {
-  const command = 'docker images --format "{ \\"id\\": {{json .ID }}, \\"created_at\\": {{ json .CreatedAt }} }"'
-  exec(command, { cwd: __dirname }, (err, stdout, stderr) => {
-    if (!err) {
-      const images = stdout
-        .split('\n')
-        .filter(line => line.length > 0)
-        .map(line => JSON.parse(line))
+  const createDockerConnection = () => {
+    if (env.socketPath) {
+      console.log(`Creating Docker socket connection via ${env.socketPath}`)
+      return new Docker({
+        socketPath: env.socketPath
+      })
+    } else {
+      console.log(`Connecting to Docker remote at ${env.host}:${env.port}`)
+      return new Docker({
+        host: env.host,
+        port: env.port
+      })
+    }
+  }
 
-      console.log(images)
+  // List of images to clean
+  const manageImages = env.images.map(image => image.name)
+
+  const docker = createDockerConnection()
+  docker.listImages((err, images) => {
+    /* Image data format
+     * {
+     *   Id: hash,
+     *   ParentId: hash,
+     *   RepoTags: [ repository:tag, ...],
+     *   RepoDigests: [ repository:tag, ...] | null,
+     *   Created: unixTime,
+     *   Size: ...,
+     *   VirtualSize: ...,
+     *   Labels: { ... }
+     * }
+     */
+
+    if (!err) {
+      // Filter for images in manageImages
+      images
+        .filter(image => image.RepoTags
+            .filter(repository => manageImages.indexOf(repository.split(':')[0]) > -1).length > 0)
+        .map(image => {
+          console.log(image)
+        })
     }
   })
 })()
